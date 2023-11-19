99f35625b001ad71dafb8691a5d51e82a5efcdc5
Gitte-Bitte <78218989+Gitte-Bitte@users.noreply.github.com>
2023-11-19 20:28:14 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+    'sofarhyd adapter settings': {
+        en: 'Adapter settings for sofarhyd',
+        de: 'Adaptereinstellungen für sofarhyd',
+        ru: 'Настройки адаптера для sofarhyd',
+        pt: 'Configurações do adaptador para sofarhyd',
+        nl: 'Adapterinstellingen voor sofarhyd',
+        fr: "Paramètres d'adaptateur pour sofarhyd",
+        it: "Impostazioni dell'adattatore per sofarhyd",
+        es: 'Ajustes del adaptador para sofarhyd',
+        pl: 'Ustawienia adaptera dla sofarhyd',
+        uk: 'Налаштування адаптера для sofarhyd',
+        'zh-cn': 'sofarhyd的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'Option 1',
+        ru: 'Опция 1',
+        pt: 'Opção 1',
+        nl: 'Optie 1',
+        fr: 'Option 1',
+        it: 'opzione 1',
+        es: 'Opción 1',
+        pl: 'opcja 1',
+        uk: 'варіант1',
+        'zh-cn': '选项1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'Option 2',
+        ru: 'вариант 2',
+        pt: 'opção 2',
+        nl: 'Optie 2',
+        fr: 'Option 2',
+        it: 'opzione 2',
+        es: 'opcion 2',
+        pl: 'Opcja 2',
+        uk: 'варіант2',
+        'zh-cn': '选项2',
+    },
+};
