4512ffe823030e853b31021a4fae3aae909fff73
Gitte-Bitte <78218989+Gitte-Bitte@users.noreply.github.com>
2023-12-11 13:37:01 +0100

    asd

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ class Sofarhyd extends utils.Adapter {
 
     splitter(resp) {
         this.log.error('splitter');
-        const result = resp.response;
+        const result = resp.response._valuesAsBuffer;
         this.log.error(typeof(resp));
         this.log.error(`hier : ${JSON.stringify(result)}`);
     }
