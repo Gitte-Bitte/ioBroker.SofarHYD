a2127c1e65710171dfb98386ca92070f4a0288f6
Gitte-Bitte <78218989+Gitte-Bitte@users.noreply.github.com>
2023-12-16 19:13:00 +0100

    fg

--- main.js
+++ main.js
@@ -228,11 +228,11 @@ class Sofarhyd extends utils.Adapter {
         //this.readFromObject();
 
         //this.makeStatesFromRegister();
-        /*
-                this.delObjectAsync('option2')
+        
+                this.delObjectAsync(this.config.option2);
                     .then((resp) => this.log.error(` geklappt: ${JSON.stringify(resp)}`))
                     .catch((resp) => this.log.error(` : Stimmt was nicht: ${JSON.stringify(resp)}`));
-                    */
+                    
         this.log.info(`config this.config: ${JSON.stringify(this.config)}`);
         /*
         await this.getObjectAsync('sofarhyd.0', true)
