ee98fdd483d0fb1b40124cd69055fb2c9f7bc419
Gitte-Bitte <78218989+Gitte-Bitte@users.noreply.github.com>
2023-12-19 19:29:02 +0100

    lkhj

--- main.js
+++ main.js
@@ -109,18 +109,18 @@ class Sofarhyd extends utils.Adapter {
                 //this.log.error(Number(r).toString() + ' ergibt zu lesen ');
                 //this.log.error(Number(r) + ' das ergibt zu lesen ');
 
-                //await client.readHoldingRegisters(Number(r), 0x40)
-                // .then((resp) => this.log.error(`Ergebnis : ${JSON.stringify(resp)}`))
-                //.then(() => this.delay(20))
+                await client.readHoldingRegisters(Number(r), 0x40)
+                .then((resp) => this.log.error(`Ergebnis : ${JSON.stringify(resp)}`))
+                .then(() => this.delay(20))
                 //.then((resp) => this.log.error(r.name + ' : wiederholt')
                 //.then((resp) => this.log.debug(r.name + ' abgerufen'))
                 //.finally(() => this.log.debug(r.name + 'Abruf erledigt'))
                 //this.log.error(`resp :  ${JSON.stringify(resp.response._body)}`);
 
-                //.catch((resp) => this.log.error(` : Stimmt was nicht: ${JSON.stringify(resp)}`));
+                .catch((resp) => this.log.error(` : Stimmt was nicht: ${JSON.stringify(resp)}`));
                 //this.log.debug(r.name + ' geschesked');
             }
-            this.log.debug('fertig mit lesen');
+            this.log.error('fertig mit lesen');
             this.setTimeout(() => { this.readFromObject(); }, 8000);
 
         }
